800e77c3bb177e8cfc9f5e70e7b1f502ac077330
zjlin-LL <zjlinliangliang@163.com>
2023-08-23 21:46:03 +0800

    0823
    Signed-off-by: zjlin-LL <zjlinliangliang@163.com>

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserBaseInfo(models.Model):
+    id=models.AutoField(verbose_name='编号',primary_key=True)
+    username = models.CharField(verbose_name='用户名称',max_length=30)
+    password = models.CharField(verbose_name='密码',max_length=20)
+    status = models.CharField(verbose_name='状态',max_length=1)
+    createdate = models.DateTimeField(verbose_name='创建日期',db_column='createDate')
